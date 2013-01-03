d6e8543baf2f6bc977da927e99ce384ed746a99a
Zhaojun Zhang <zhangzhaojun@gmail.com>
2013-01-03 14:34:18 -0500

    A simple implememtation of rached.memoise
    Use local filesystem as storage.
    Add test.R to test the functions.

--- rached.R
+++ rached.R
@@ -1,9 +1,36 @@
-memoise <- function(func, version=0, md5=F) {
-  new.func <- function(...) {
+
+rached.folder <- "temp.folder/"
+
+rached.initialize <- function() {
+  if (!file.exists(rached.folder)) {
+    dir.create(rached.folder)
+  }
+}
+
+rached.clear <- function() {
+  unlink(paste(rached.folder, "*", sep=""))
+}
+
+rached.initialize()
+
+rached.memoise <- function(func, name, version=0, md5=F) {
+  key <- function(name, version, md5, ...) {
     parameters = list(...)
-    print(names(parameters))
-    print(version)
-    func(...)
+    k <- paste(name, version, paste(names(parameters), collapse="-"),
+               paste(parameters, collapse="-"), sep=":")
+    return(k)
+  }
+  new.func <- function(...) {
+    k <- key(name, version, md5, ...)
+    data.file <- paste(rached.folder, "/", k, ".rds", sep="")
+    if (!file.exists(data.file)) {
+      ret <- func(...)
+      saveRDS(ret, data.file)
+    }
+    else {
+      ret <- readRDS(data.file)
+    }
+    return (ret)
   }
   return (new.func)
 }
--- test.R
+++ test.R
@@ -1,16 +1,28 @@
 source('rached.R')
-a <- function(b) {
-  return(b)
+rached.clear()
+
+
+## a function to make sure each function is only called once.
+called.funcs <- new.env()
+called <- function(name) {
+  stopifnot(!exists(name, called.funcs, inherits=F))
+  assign(name, T, called.funcs, inherits=F)
 }
 
-a.cached <- memoise(a)
+a <- function(b) {
+  called('a')
+  return(b)
+}
+a.cached <- rached.memoise(a, 'a')
+a.cached <- rached.memoise(a, 'a')
+
 stopifnot(3 == a.cached(3))
 
 b <- function(x, y, z) {
+  called('b')
   return (x + y + z)
 }
+b.cached <- rached.memoise(b, 'b')
 
-b.cached <- memoise(b)
 stopifnot(6 == b.cached(1, 2, 3))
-b.cached <- memoise(b, version=3)
 stopifnot(6 == b.cached(1, 2, 3))
