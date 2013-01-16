fe849bccc5a0ba9ab2847b3101ec0ac341aa5baa
Zhaojun Zhang <zhangzhaojun@gmail.com>
2013-01-16 11:15:01 -0500

    add function to remove old cached file

--- rached.R
+++ rached.R
@@ -18,20 +18,32 @@ rached.legalize.filename <- function(k) {
 }
 
 
+rached.key <- function(name, version, md5, ...) {
+  parameters = list(...)
+  k <- paste(name, version, paste(names(parameters), collapse="-"),
+             paste(parameters, collapse="-"), sep=":")
+  return(k)
+}
+
+rached.file <- function(name, version, md5, ...) {
+  k <- rached.key(name, version, md5, ...)
+  legal.k <- rached.legalize.filename(k)
+  paste(rached.folder, "/", legal.k, ".rds", sep="")
+}
+
 rached.memoise <- function(func, name, version=0, md5=F) {
-  key <- function(name, version, md5, ...) {
-    parameters = list(...)
-    k <- paste(name, version, paste(names(parameters), collapse="-"),
-               paste(parameters, collapse="-"), sep=":")
-    return(k)
-  }
   new.func <- function(...) {
-    k <- key(name, version, md5, ...)
-    legal.k <- rached.legalize.filename(k)
-    data.file <- paste(rached.folder, "/", legal.k, ".rds", sep="")
+    data.file <- rached.file(name, version, md5, ...)
     if (!file.exists(data.file)) {
       ret <- func(...)
       saveRDS(ret, data.file)
+      ## remove old cached data
+      if (version > 0) {
+        old.data.file <- rached.file(name, version - 1, md5, ...)
+        if (file.exist(old.data.file)) {
+          unlink(old.data.file)
+        }
+      }
     }
     else {
       ret <- readRDS(data.file)
